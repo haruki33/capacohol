cb823183dd7d1729e742a8d8e728a449488dc291
haruki <kkhk0869hs33@gmail.com>
2024-08-28 12:01:15 +0900

    レイアウト〇 履歴表示：日付指定しても何も起こらず

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -3,49 +3,36 @@
     <div class="ui main container">
       <!-- 基本的なコンテンツはここに記載する -->
       <h1 class="ui dividing header">お酒カレンダー</h1>
-      <div class="ui one column grid" style="width: 100%; margin: 0">
-        <div
-          class="column"
-          style="
-            text-align: center;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-          "
-        >
-          <VDatePicker
-            class="cal"
-            v-model.string="selectedDate"
-            :masks="masks"
-          />
-        </div>
+      <div class="ui segment calendar-segment">
+        <VDatePicker expanded v-model.string="selectedDate" :masks="masks" />
+      </div>
 
-        <h3 class="ui dividing header">{{ selectedDate }} の飲酒記録</h3>
-        <div class="ui segment">
-          <ul class="ui comments divided alcohol-list">
-            <template v-for="(record, index) in records" :key="index">
-              <li class="comment">
-                <div class="content">
-                  <button
-                    v-if="isMyAlcohol(record.userId)"
-                    class="ui negative mini button right floated"
-                    @click="deleteAlcohol(record)"
-                  >
-                    削除
-                  </button>
-                  <p class="text">
-                    アルコール度数: {{ record.alcoholContent }}%、 飲んだ量:
-                    {{ record.alcoholQuantity }}ml、 本数:
-                    {{ record.alcoholNum }}
-                  </p>
-                  <span class="ui green label">
-                    酔い度: {{ record.currentIntoxicationLevel }}</span
-                  >
-                  <div class="ui divider"></div>
-                </div>
-              </li>
-            </template>
-          </ul>
+      <!-- 履歴標示 -->
+      <div class="ui segment record">
+        <div class="ui comments">
+          <h3 class="ui dividing header">{{ selectedDate }} の飲酒記録</h3>
+          <template v-for="(record, index) in records" :key="index">
+            <div class="ui comment">
+              <div class="content">
+                <button
+                  v-if="isMyAlcohol(record.userId)"
+                  class="ui negative mini button right floated"
+                  @click="deleteAlcohol(record)"
+                >
+                  削除
+                </button>
+                <p class="text">
+                  アルコール度数: {{ record.alcoholContent }}%、 飲んだ量:
+                  {{ record.alcoholQuantity }}ml、 本数:
+                  {{ record.alcoholNum }}
+                </p>
+                <span class="ui green label">
+                  酔い度: {{ record.currentIntoxicationLevel }}</span
+                >
+                <div class="ui divider"></div>
+              </div>
+            </div>
+          </template>
         </div>
       </div>
     </div>
@@ -160,34 +147,16 @@ export default {
 <style scoped>
 /* このコンポーネントだけに適用するCSSはここに記述する */
 .ui.main.container {
-  background: #fff;
-  border-radius: 4px;
-  padding: 20px;
-  transition: all 0.2s;
-  text-align: center;
   margin-bottom: 100px; /* メニューバーの高さと同じ値に設定 */
   padding-bottom: 100px; /* メニューバーの高さを余白として追加 */
 }
 
-.ui.main.container h1 {
-  margin-bottom: 20px; /* 下部マージンを追加 */
-  text-align: left;
-}
-
-.custom-calendar {
-  /* カレンダーのサイズを調整 */
-  width: 100%; /* 幅を100%に設定 */
-  max-width: 800px; /* 最大幅を800pxに設定 */
-  margin: 0 auto; /* 中央に配置 */
-  font-size: 1.5em; /* フォントサイズを大きく設定 */
+.ui.comments.divided.alcohol-list {
+  list-style-type: none;
 }
 
 .cal {
-  width: 100%;
-  margin: 0;
-}
-
-.ui.comments.divided.alcohol-list {
-  list-style-type: none;
+  justify-content: center;
+  align-items: center;
 }
 </style>
